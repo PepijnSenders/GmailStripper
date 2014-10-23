52e823fc59e99d5c8a8e39b847c0b43020770471
Pepijn Senders <p.senders92@gmail.com>
2014-10-23 10:33:20 +0200

    added new tests

--- tests/GmailStripperTest.php
+++ tests/GmailStripperTest.php
@@ -6,19 +6,19 @@ class GmailStripperTest extends PHPUnit_Framework_TestCase {
 
   public function testEmails() {
     $emails = [
-      'p.senders92@gmail.com',
-      'psenders92@gmail.com',
-      'p.s.e.n.d.e.r.s.9.2@gmail.com',
-      'p.senders92+teststest@gmail.com',
-      'psenders92+teststest@gmail.com',
-      'p.s.e.n.d.e.r.s.9.2+teststest@gmail.com',
-      'p.senders92+teststest+sdaasddsa@gmail.com',
-      'psenders92+teststest+sdaasddsa@gmail.com',
-      'p.s.e.n.d.e.r.s.9.2+teststest+sdaasddsa@gmail.com',
+      'p.ep@gmail.com',
+      'pep@gmail.com',
+      'p.e.p@gmail.com',
+      'p.ep+teststest@gmail.com',
+      'pep+teststest@gmail.com',
+      'p.e.p+teststest@gmail.com',
+      'p.ep+teststest+sdaasddsa@gmail.com',
+      'pep+teststest+sdaasddsa@gmail.com',
+      'p.e.p+teststest+sdaasddsa@gmail.com',
     ];
 
     foreach ($emails as $email) {
-      $this->assertEquals(GmailStripper::strip($email), 'psenders92@gmail.com');
+      $this->assertEquals(GmailStripper::strip($email), 'pep@gmail.com');
     }
   }
 
